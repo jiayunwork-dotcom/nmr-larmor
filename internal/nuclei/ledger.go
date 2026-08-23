a2e847c8661828e964b1b6b153d1e1b80f549c9a
“jiayun” <jiayun_work@163.com>
2026-08-23 21:09:18 +0800

    fix: LookupStrict 写入前 make nil map

--- internal/nuclei/ledger.go
+++ internal/nuclei/ledger.go
@@ -9,5 +9,8 @@ type LookupLedger struct {
 var defaultLookupLedger = &LookupLedger{}
 
 func registerLookup(sym string) {
+	if defaultLookupLedger.bySym == nil {
+		defaultLookupLedger.bySym = make(map[string]bool)
+	}
 	defaultLookupLedger.bySym[sym] = true
 }
